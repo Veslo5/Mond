2c08bd3756b464ebc4506807a2a82be5d44f9ce7
Rohan Singh <rohan-singh@hotmail.com>
2025-06-03 21:57:09 -0400

    Improved completion for standard library members

--- Mond.VSCode/src/activateCompletionProvider.ts
+++ Mond.VSCode/src/activateCompletionProvider.ts
@@ -1,5 +1,33 @@
 import * as vscode from "vscode";
-import { keywords, stlMethods, stlConstants } from "./completionItems"
+import { keywords, methods, constants } from "./completionItems";
+
+const keywordCompletionItems = keywords.map((keyword) => {
+    const item = new vscode.CompletionItem(
+        keyword,
+        vscode.CompletionItemKind.Keyword,
+    );
+    return item;
+});
+
+const constantCompletionItems = constants.map((method) => {
+    const item = new vscode.CompletionItem(
+        method,
+        vscode.CompletionItemKind.Constant,
+    );
+
+    return item;
+});
+
+const methodCompletionItems = methods.map((method) => {
+    const item = new vscode.CompletionItem(
+        method,
+        vscode.CompletionItemKind.Method,
+    );
+
+    return item;
+});
+
+const allCompletionItems = [...keywordCompletionItems, ...methodCompletionItems, ...constantCompletionItems];
 
 export function activateCompletionProvider(context: vscode.ExtensionContext) {
     context.subscriptions.push(        
@@ -10,40 +38,32 @@ export function activateCompletionProvider(context: vscode.ExtensionContext) {
                     document: vscode.TextDocument,
                     position: vscode.Position
                 ) {
-                    const keywordCompletionItems = keywords.map((keyword) => {
-                        const item = new vscode.CompletionItem(
-                            keyword,
-                            vscode.CompletionItemKind.Keyword
-                        );
-                        return item;
-                    });
-
                     const config = vscode.workspace.getConfiguration('mond.standardLibraries');
                     const stlEnableCompletion = config.get<boolean>('enableCompletion');
-                    
-                    if (!stlEnableCompletion) {
-                        return [... keywordCompletionItems];
+
+                    const wordRange = document.getWordRangeAtPosition(position, /(?:(?:[A-Z]\w+\.)(?:[A-Za-z_]\w*)?)|(?:(?<!\.)\b[A-Za-z_]\w*)/g);
+                    if (!wordRange) {
+                        return [];
                     }
-
-                    const constantItems = stlConstants.map((method) => {
-                        const item = new vscode.CompletionItem(
-                            method,
-                            vscode.CompletionItemKind.Constant
-                        );
-
-                        return item;
-                    });
-
-                    const methodItems = stlMethods.map((method) => {
-                        const item = new vscode.CompletionItem(
-                            method,
-                            vscode.CompletionItemKind.Method
-                        );
-
-                        return item;
-                    });
-
-                    return [...keywordCompletionItems, ...methodItems, ...constantItems];
+                    
+                    const word = document.getText(wordRange);
+                    
+                    const completionItems = stlEnableCompletion
+                        ? allCompletionItems
+                        : keywordCompletionItems;
+                    
+                    // VSCode filters the completion items for us in all cases except manually triggering completion after typing something like "Char."
+                    // It filters fine when automatically triggered but manually triggering after text like that makes it not filter at all
+                    // Probably because the text ends with a dot so it assumes it doesn't matter
+                    if (word && typeof word === "string") {
+                        const dotIndex = word.indexOf(".");
+                        if (dotIndex > 0) {
+                            const moduleName = word.substring(0, dotIndex);
+                            return completionItems.filter(i => typeof i.label === "string" && i.label.startsWith(moduleName));
+                        }
+                    }
+                    
+                    return completionItems;
                 },
             }
         )
--- Mond.VSCode/src/completionItems.ts
+++ Mond.VSCode/src/completionItems.ts
@@ -27,108 +27,116 @@ export const keywords: string[] = [
     "Infinity"
 ];
 
-export const stlConstants: string[] = [
-    //Math module
-    "PI",
-    "E"
-];
+const moduleConstants: { [module: string]: string[] } = {
+    "Math": [
+        "PI",
+        "E",
+    ],
+};
 
-export const stlMethods: string[] = [
-    //Require module
+export const constants = Object.entries(moduleConstants).flatMap(([module, members]) => members.map(m => `${module}.${m}`));
+
+const standaloneMethods: string[] = [
+    // Require module
     "require",
 
-    //Proxy module
+    // Proxy module
     "proxyCreate",
 
-    //Error module
+    // Error module
     "error",
     "try",
 
-    //Parse module
+    // Parse module
     "parseFloat",
     "parseInt",
     "parseHex",
 
-    //Char module
-    "toNumber",
-    "fromNumber",
-    "convertFromUtf32",
-    "convertToUtf32",
-    "getNumericValue",
-    "getUnicodeCategory",
-    "isControl",
-    "isDigit",
-    "isHighSurrogate",
-    "isLetter",
-    "isLetterOrDigit",
-    "isLower",
-    "isLowSurrogate",
-    "isNumber",
-    "isPunctuation",
-    "isSeparator",
-    "isSurrogate",
-    "isSurrogatePair",
-    "isSymbol",
-    "isUpper",
-    "isWhiteSpace",
-
-    //Math module
-    "abs",
-    "acos",
-    "asin",
-    "atan",
-    "atan2",
-    "ceiling",
-    "clamp",
-    "cos",
-    "cosh",
-    "exp",
-    "floor",
-    "log",
-    "log10",
-    "max",
-    "min",
-    "pow",
-    "round",
-    "sign",
-    "sin",
-    "sinh",
-    "sqrt",
-    "tan",
-    "tanh",
-    "truncate",
-
-    //Random module
-    "next",
-    "nextDouble",
-
-    //Console Output module
+    // Console Output module
     "print",
     "printLn",
 
-    //Console Input module
+    // Console Input module
     "readLn",
 
-    //Json module
-    "serialize",
-    "deserialize",
-
-    //Async module
-    "start",
-    "run",
-    "runToCompletion",
-    "delay",
-    "whenAll",
-    "whenAny",
-    "getTask",
-    "setCanceled",
-    "setException",
-    "setResult",
-    "isCancellationRequested",
-    "getToken",
-    "cancel",
-    "cancelAfter",
-    "isCancellationRequested",
-    "register",
-    "throwIfCancellationRequested"
+    // Random module
+    "Random",
 ];
+
+const moduleMethods: { [module: string]: string[] } = {
+    "Char": [
+        "toNumber",
+        "fromNumber",
+        "convertFromUtf32",
+        "convertToUtf32",
+        "getNumericValue",
+        "getUnicodeCategory",
+        "isControl",
+        "isDigit",
+        "isHighSurrogate",
+        "isLetter",
+        "isLetterOrDigit",
+        "isLower",
+        "isLowSurrogate",
+        "isNumber",
+        "isPunctuation",
+        "isSeparator",
+        "isSurrogate",
+        "isSurrogatePair",
+        "isSymbol",
+        "isUpper",
+        "isWhiteSpace",
+    ],
+    "Math": [
+        "abs",
+        "acos",
+        "asin",
+        "atan",
+        "atan2",
+        "ceiling",
+        "clamp",
+        "cos",
+        "cosh",
+        "exp",
+        "floor",
+        "log",
+        "log10",
+        "max",
+        "min",
+        "pow",
+        "round",
+        "sign",
+        "sin",
+        "sinh",
+        "sqrt",
+        "tan",
+        "tanh",
+        "truncate",
+    ],
+    "Json": [
+        "serialize",
+        "deserialize",
+    ],
+    "Async": [
+        "start",
+        "run",
+        "runToCompletion",
+        "delay",
+        "whenAll",
+        "whenAny",
+        "getTask",
+        "setCanceled",
+        "setException",
+        "setResult",
+        "isCancellationRequested",
+        "getToken",
+        "cancel",
+        "cancelAfter",
+        "isCancellationRequested",
+        "register",
+        "throwIfCancellationRequested",
+    ],
+};
+
+const moduleMethodsFlattened = Object.entries(moduleMethods).flatMap(([module, members]) => members.map(m => `${module}.${m}`));
+export const methods = standaloneMethods.concat(moduleMethodsFlattened);
